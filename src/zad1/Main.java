19e0d5eebac37e091ad685f9708d98d309df2575
majki <michal.m.baran@gmail.com>
2016-05-11 18:44:03 +0200

    zad1 done

--- src/zad1/Main.java
+++ src/zad1/Main.java
@@ -1,14 +1,34 @@
 /**
- *
- *  @author Baran Michał PD2067
- *
+ * @author Baran Michał PD2067
  */
 
 package zad1;
 
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
-  public static void main(String[] args) {
-  }
+    public Main() {
+        JList list = new JList(new MyModel());
+        JScrollPane scroll = new JScrollPane(list);
+        JFrame f = new JFrame();
+        GridLayout lay = new GridLayout();
+        f.setLayout(lay);
+        f.setVisible(true);
+        f.add(scroll);
+        f.pack();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Main();
+            }
+        });
+
+    }
 }
---
+++ src/zad1/MyModel.java
@@ -0,0 +1,33 @@
+package zad1;
+
+import javax.swing.*;
+
+/**
+ * Created by majki on 11.05.2016.
+ */
+public class MyModel extends AbstractListModel {
+    int start;
+    int finish;
+
+    MyModel() {
+
+        start = -70;
+        finish = 60;
+
+    }
+
+    @Override
+    public int getSize() {
+        return finish - start;
+    }
+
+    @Override
+    public Object getElementAt(int index) {
+        int celc = start + index;
+        int fahr = 9*celc/5 + 32;
+
+        String pozycja = celc +" stopni C = " + fahr +  "stopni F";
+
+        return pozycja;
+    }
+}
